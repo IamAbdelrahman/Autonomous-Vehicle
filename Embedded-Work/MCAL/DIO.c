4ec624b57d28f40c5080746418bc9beb7014cca3
Abdelrahman Kamal <abdelruhman.m.kamal@gmail.com>
2023-02-16 17:27:37 +0200

    Team Embedded Work

--- Embedded-Work/MCAL/DIO.c
+++ Embedded-Work/MCAL/DIO.c
@@ -1,12 +1,11 @@
-#include "DIO_H"
+#include "DIO.h"
 
-void DIO_init (uint8_t portName, uint8_t pinNumber, uint8_t direction) 
-{
-    switch (portName) 
-    {
-
+void DIO_Init_Pin(uint8_t portName, uint8_t pinNumber, uint8_t direction) {
+    
+    switch (portName) {
+        
         case 'A':
-
+            
             if (direction == OUT)
                 SET_BIT(DDRA, pinNumber);
 
@@ -16,6 +15,7 @@ void DIO_init (uint8_t portName, uint8_t pinNumber, uint8_t direction)
             break;
 
         case 'B':
+            
             if (direction == OUT)
                 SET_BIT(DDRB, pinNumber);
 
@@ -25,6 +25,7 @@ void DIO_init (uint8_t portName, uint8_t pinNumber, uint8_t direction)
             break;
 
         case 'C':
+            
             if (direction == OUT)
                 SET_BIT(DDRC, pinNumber);
 
@@ -34,6 +35,7 @@ void DIO_init (uint8_t portName, uint8_t pinNumber, uint8_t direction)
             break;
 
         case 'D':
+            
             if (direction == OUT)
                 SET_BIT(DDRD, pinNumber);
 
@@ -41,37 +43,38 @@ void DIO_init (uint8_t portName, uint8_t pinNumber, uint8_t direction)
                 CLR_BIT(DDRD, pinNumber);
 
             break;
-            
+
         default:
-            
             break;
     }
 }
 
-void DIO_write (uint8_t portName, uint8_t pinNumber, uint8_t value) 
-{
-    switch (portName) 
-    {
+void DIO_Write_Pin(uint8_t portName, uint8_t pinNumber, uint8_t value) {
+    
+    switch (portName) {
+        
         case 'A':
-
+            
             if (value == HIGH)
                 SET_BIT(PORTA, pinNumber);
 
             else if (value == LOW)
                 CLR_BIT(PORTA, pinNumber);
-            
+
             break;
 
         case 'B':
+            
             if (value == HIGH)
                 SET_BIT(PORTB, pinNumber);
 
             else if (value == LOW)
                 CLR_BIT(PORTB, pinNumber);
-            
+
             break;
 
         case 'C':
+            
             if (value == HIGH)
                 SET_BIT(PORTC, pinNumber);
 
@@ -81,6 +84,7 @@ void DIO_write (uint8_t portName, uint8_t pinNumber, uint8_t value)
             break;
 
         case 'D':
+            
             if (value == HIGH)
                 SET_BIT(PORTD, pinNumber);
 
@@ -88,79 +92,114 @@ void DIO_write (uint8_t portName, uint8_t pinNumber, uint8_t value)
                 CLR_BIT(PORTD, pinNumber);
 
             break;
-            
+
         default:
-            
             break;
     }
 }
 
-void DIO_toggle (uint8_t portName, uint8_t pinNumber) //toggle dio pins
-{
+void DIO_Toggle_Pin(uint8_t portName, uint8_t pinNumber){
+    
     switch (portName) {
-
+        
         case 'A':
             
             TOGGLE_BIT(PORTA, pinNumber);
-            
             break;
 
         case 'B':
             
             TOGGLE_BIT(PORTB, pinNumber);
-            
             break;
 
         case 'C':
             
             TOGGLE_BIT(PORTC, pinNumber);
-            
             break;
 
         case 'D':
             
             TOGGLE_BIT(PORTD, pinNumber);
-            
             break;
-            
+
         default:
-            
             break;
     }
 }
 
-void DIO_read (uint8_t portName, uint8_t pinNumber, uint8_t* value) {
+void DIO_Read_Pin(uint8_t portName, uint8_t pinNumber, uint8_t* pinValue) {
+    
     switch (portName) {
-
+        
         case 'A':
-
-            *value = READ_BIT(PINA, pinNumber);
-
+            
+            *pinValue = READ_BIT(PINA, pinNumber);
             break;
 
         case 'B':
-
-            *value = READ_BIT(PINB, pinNumber);
-
+            
+            *pinValue = READ_BIT(PINB, pinNumber);
             break;
 
         case 'C':
-
-            *value = READ_BIT(PINC, pinNumber);
-
+            
+            *pinValue = READ_BIT(PINC, pinNumber);
             break;
 
         case 'D':
-
-            *value = READ_BIT(PIND, pinNumber);
-
+            
+            *pinValue = READ_BIT(PIND, pinNumber);
             break;
-            
+
         default:
-            
             break;
     }
-
 }
 
+void DIO_Port_Direction(UGPIO_t* Ptr, uint8_t portDirection) {
+    
+    if (portDirection == OUT)
+        Ptr-> All_Bits = 0xFF;
+
+    else if (portDirection == IN)
+        Ptr-> All_Bits = 0x00;
+}
+
+void DIO_Port_Write(UGPIO_t* Ptr, uint8_t portValue) {
+    
+    if (portValue == LOW)
+        Ptr-> All_Bits = 0x00;
+
+    else if (portValue == HIGH)
+        Ptr-> All_Bits = 0xFF;
+}
+
+void DIO_Port_Read(uint8_t portName, uint8_t* portValue) {
+    
+    switch (portName) {
+        
+        case 'A':
+            
+            *portValue = PINA;
+            break;
+
+        case 'B':
+            
+            *portValue = PINB;
+            break;
+
+        case 'C':
+            
+            *portValue = PINC;
+            break;
+
+        case 'D':
+            
+            *portValue = PIND;
+            break;
+
+        default:
+            break;
+    }
+}
 
--- Embedded-Work/MCAL/DIO.h
+++ Embedded-Work/MCAL/DIO.h
@@ -1,5 +1,5 @@
 /* 
- * File:   GPIO.h
+ * File:   DIO.h
  * Author: ak
  *
  * Created on February 12, 2023, 12:16 AM
@@ -7,6 +7,7 @@
 
 #ifndef DIO_H
 #define	DIO_H
+
 #include "Registers.h"
 #include "Platform_Types.h"
 
@@ -16,7 +17,28 @@
 #define HIGH 1
 #define LOW 0
 
-/***************************DIO_init**********************************
+typedef struct{
+   
+        vuint8_t pin0 : 1;
+        vuint8_t pin1 : 1;
+        vuint8_t pin2 : 1;
+        vuint8_t pin3 : 1;
+        vuint8_t pin4 : 1;
+        vuint8_t pin5 : 1;
+        vuint8_t pin6 : 1;
+        vuint8_t pin7 : 1;
+        
+    }SGPIO_t;
+    
+typedef union{
+        
+        vuint8_t All_Bits;
+        SGPIO_t PORTS;
+        
+    }UGPIO_t;
+    
+    
+/***************************DIO_Init_Pin**********************************
  * This function initialize the pins of the port registers
  * through configuring the pin as an output pin or an input pin
  *
@@ -25,63 +47,126 @@
  * name of the ports of the microcontroller.
  *(pinNumber): It represents the pin number of each port
  * and takes a number from 0 : 7.
- * (direction): It represents the state of the pin
- * whether it's out or in and you can use numbers 0 or 1
+ * (pinDirection): It represents the state of the pin
+ * whether it's OUT or IN and you can use numbers 0 or 1
  * otherwise you can use the macro directly for well-readability
  * 
  * Return: Void
- ***************************DIO_init**********************************/
-void DIO_init (uint8_t portName, uint8_t pinNumber, uint8_t direction);	
+ ***************************DIO_Init_Pin**********************************/
+void DIO_Init_Pin (uint8_t portName, uint8_t pinNumber, uint8_t pinDirection);	
 
 
-/***************************DIO_write**********************************
+/***************************DIO_Write_Pin**********************************
  * This function is used to pull each pin high or low of the port registers
  * through writing on it 1(high) or 0(low).
  *
  * Inputs: (portName): This is a character. It must be one of 
  * these characters 'A', 'B', 'C', and 'D' as they represent the
  * name of the ports of the microcontroller.
+ * 
  *(pinNumber): It represents the pin number of each port
  * and takes a number from 0 : 7.
- * (value): It represents the state of the pin
- * whether it's high or low and you can use numbers 0 or 1
+ * 
+ * (pinValue): It represents the state of the pin
+ * whether it's HIGH or LOW and you can use numbers 0 or 1
  * otherwise you can use the macro directly for well-readability
  * 
  * Return: Void
-***************************DIO_write**********************************/
-void DIO_write (uint8_t portName, uint8_t pinNumber, uint8_t value); 
+***************************DIO_Write_Pin**********************************/
+void DIO_Write_Pin (uint8_t portName, uint8_t pinNumber, uint8_t pinValue); 
 
 
-/***************************DIO_toggle**********************************
- * This function is used to toggle each pin from high to low
+/***************************DIO_Toggle_Pin**********************************
+ * This function is used to toggle each pin from high to low and vice versa
  * through using the toggle_bit equation (defined in registers file).
  *
  * Inputs: (portName): This is a character. It must be one of 
  * these characters 'A', 'B', 'C', and 'D' as they represent the
  * name of the ports of the microcontroller.
+ * 
  *(pinNumber): It represents the pin number of each port
  * and takes a number from 0 : 7.
  * 
  * Return: Void
-***************************DIO_toggle**********************************/
-void DIO_toggle (uint8_t portName, uint8_t pinNumber); 
+***************************DIO_Toggle_Pin**********************************/
+void DIO_Toggle_Pin (uint8_t portName, uint8_t pinNumber); 
 
 
-/***************************DIO_read**********************************
+/***************************DIO_Read_Pin**********************************
  * This function is used to know the status of each pin high or low of the 
  * port registers through reading from it 1(high) or 0(low).
  *
  * Inputs: (portName): This is a character. It must be one of 
  * these characters 'A', 'B', 'C', and 'D' as they represent the
  * name of the ports of the microcontroller.
+ * 
  *(pinNumber): It represents the pin number of each port
  * and takes a number from 0 : 7.
- * (*value): This pointer will store the state of the pin
- * whether it's high or low ( 0 or 1) by reading from each pin
+ * 
+ * (pinValue): This pointer will store the state of the pin
+ * whether it's HIGH or LOW ( 0 or 1) by reading from each pin
  * 
  * Return: Void
-***************************DIO_read**********************************/
-void DIO_read (uint8_t portName, uint8_t pinNumber, uint8_t* value); 
+***************************DIO_Read_Pin**********************************/
+void DIO_Read_Pin (uint8_t portName, uint8_t pinNumber, uint8_t* pinValue); 
 
+
+/***************************DIO_Port_Init**********************************
+ * This function initializes the entire ddrx registers
+ * through configuring the all pins as output pins or input pins
+ * by using the structures, unions, and bit-fields method to access
+ * the register.
+ *
+ * Inputs: (pGPIO): pGPIO is a pointer to union, and this union
+ * includes a structure that includes all the bits of any I/O port
+ * in the gpio section in the microcontroller in addition that including
+ * a variable (all-bits).
+ * 
+ * (portDirection): It represents the state of the port
+ * whether it's OUT or IN and you can use numbers 0 or 1
+ * otherwise you can use the macro directly for well-readability
+ * 
+ * Return: Void
+***************************DIO_Port_Init**********************************/
+void DIO_Port_Init (UGPIO_t* pGPIO, uint8_t portDirection);
+
+
+/***************************DIO_Port_Write**********************************
+ * This function writes on the entire port registers
+ * through writing on the all pins as high pins or low pins
+ * by using the structures, unions, and bit-fields method to access
+ * the register.
+ *
+ * Inputs: (pGPIO): pGPIO is a pointer to union, and this union
+ * includes a structure that includes all the bits of any I/O port
+ * in the GPIO section in the microcontroller in addition that including
+ * a variable (all-bits).
+ * 
+ * (portValue): It represents the state of the port
+ * whether it's HIGH or LOW as an output or an input 
+ * and you can use numbers 0 or 1, otherwise you can use the 
+ * macro directly for well-readability
+ * 
+ * Return: Void
+***************************DIO_Port_Write**********************************/
+void DIO_Port_Write (UGPIO_t* pGPIO, uint8_t portValue);
+
+
+/***************************DIO_Port_Read**********************************
+ * This function reads from the entire pinx registers
+ * through reading the all pins as high pins or low pins
+ * by using the structures, unions, and bit-fields method to access
+ * the register.
+ *
+ * Inputs: (portName): This is a character. It must be one of 
+ * these characters 'A', 'B', 'C', and 'D' as they represent the
+ * name of the ports of the microcontroller.
+ * 
+ * (portValue): This pointer will store the state of the pinx
+ * whether it's HIGH or LOW ( 0 or 1) by reading it totally
+ * 
+ * Return: Void
+***************************DIO_Port_Read**********************************/
+void DIO_Port_Read (uint8_t portName, uint8_t* portValue);
 #endif	/* DIO_H */
 
