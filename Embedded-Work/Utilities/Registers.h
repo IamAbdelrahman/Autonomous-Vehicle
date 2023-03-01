6f8fe66dead0de73aab0d0e8140883cccb46d17b
Abdelrahman Kamal <abdelruhman.m.kamal@gmail.com>
2023-03-01 23:38:05 +0200

    Team Embedded Work

--- Embedded-Work/Utilities/Registers.h
+++ Embedded-Work/Utilities/Registers.h
@@ -56,23 +56,30 @@
 #define PIN_D    0x30
 
 
-// TIMER - Registers
-#define TCCR0    0x53	
-#define TCNT0    *((vuint8_t*)0x52)	
-#define TIFR     *((vuint8_t*)0x58)
-#define OCR0     *((vuint8_t*)0x5C)
-#define TIMSK    0x59
-#define TCCR1A   0x4F
-#define TCCR1B   0x4E
+// TIMER0 - Registers
+#define TCCR0       0x53	
+#define TCNT0       *((vuint8_t*)0x52)	
+#define TIFR        *((vuint8_t*)0x58)
+#define OCR0        *((vuint8_t*)0x5C)
+
+#define TIMSK       0x59
+
+// TIMER1 - Registers
+#define TCCR1A      0x4F
+#define TCCR1B      0x4E
 #define TCNT1H      *((vuint8_t*)0x4D)
 #define TCNT1L      *((vuint8_t*)0x4C)
 #define OCR1AH      *((vuint8_t*)0x4B)
 #define OCR1AL      *((vuint8_t*)0x4A)
-#define ICR1H       *
+#define OCR1BH      *((vuint8_t*)0x49)
+#define OCR1BL      *((vuint8_t*)0x48)
+#define ICR1H       *((vuint8_t*)0x47)
+#define ICR1L       *((vuint8_t*)0x46)
 
-#define TCCR2    0x45
-#define TCNT2    *((vuint8_t*)0x44)	
-#define OCR2     *((vuint8_t*)0x43)
+// TIMER2 - Registers
+#define TCCR2       0x45
+#define TCNT2       *((vuint8_t*)0x44)	
+#define OCR2        *((vuint8_t*)0x43)
 
 #define IN 0
 #define OUT 1
