28129f540903aaad5271ea38c1c8d8c9d1c86423
Abdelrahman Kamal <abdelruhman.m.kamal@gmail.com>
2023-04-14 19:57:55 +0200

    Team Embedded Work

--- Embedded-Work/MCAL/UART/UART.c
+++ Embedded-Work/MCAL/UART/UART.c
@@ -1,41 +1,67 @@
 #include "UART.h"
 
-void USART_Init( uint32_t baud )
-{
-/* Set baud rate */
-UBRRH = (uint8_t)(baud>>8);
-UBRRL = (uint8_t)baud;
-// UBRRL = BAUD_PRESCALE;
-/* Enable receiver and transmitter */
-UCSRB = (1<<4)|(1<<3);
+ST_UART_config_t S_UART = {BAUD_PRESCALE, TX_RX_ENABLE, DATA_BITS_8,
+    PARITY_MODE_DISABLED, STOP_BITS_2, NORMAL_SPEED};
+
+volatile UUCSRA_t * const pUCSRA = ADDRESS_UCSRA;
+volatile UUCSRB_t * const pUCSRB = ADDRESS_UCSRB;
+volatile UUCSRC_t * const pUCSRC = ADDRESS_UCSRC;
+
+void UART_TX_RX_Enable(ST_UART_config_t *pUART) {
+    if (pUART-> enableTXorRX == TX_ENABLE) {
+        pUCSRB->Ucsrb.TXEN = 1;
+    } else if (pUART-> enableTXorRX == RX_ENABLE) {
+        pUCSRB->Ucsrb.RXEN = 1;
+    } else if (pUART->enableTXorRX == TX_RX_ENABLE) {
+        pUCSRB->Ucsrb.RXEN = 1;
+        pUCSRB->Ucsrb.TXEN = 1;
+    }
+}
+
+void UART_Data_Size(ST_UART_config_t *pUART) {
+    pUCSRC->Ucsrc.URSEL = 1; // Enalbe accessing the USCRC Register.
+    
+    if (pUART->characterSize == DATA_BITS_5) {
+        
+    } else if (pUART->characterSize == DATA_BITS_6) {
+
+    } else if (pUART->characterSize == DATA_BITS_6) {
+
+    } else if (pUART->characterSize == DATA_BITS_6) {
+
+    }
+}
+
+void UART_Initialize(uint64_t Baud_Rate) {
+    /* Set baud rate */
+    UBRRL = BAUD_PRESCALE;
+    UBRRH = (BAUD_PRESCALE >> 8);
+}
+
 /* Set frame format: 8data, 2stop bit */
-UCSRC = (1<<7)|(1<<3)|(3<<1);
+UCSRC = (1 << 7) | (1 << 3) | (3 << 1);
 }
 
-void USART_Transmit( uint8_t data )
-{
-/* Wait for empty transmit buffer */
-while ( !( UCSRA & (1<<5)) );
-/* Put data into buffer, sends the data */
-UDR = data;
+void USART_Transmit(uint8_t data) {
+    /* Wait for empty transmit buffer */
+    while (!(UCSRA & (1 << 5)));
+    /* Put data into buffer, sends the data */
+    UDR = data;
 }
 
-uint8_t USART_Receive( void )
-{
-/* Wait for data to be received */
-while ( !(UCSRA & (1<<7)) )
-;
-/* Get and return received data from buffer */
-return UDR;
+uint8_t USART_Receive(void) {
+    /* Wait for data to be received */
+    while (!(UCSRA & (1 << 7)))
+        ;
+    /* Get and return received data from buffer */
+    return UDR;
 }
 
-void USART_SendString(char *str)
-{
-	unsigned char j=0;
-	
-	while (str[j]!=0)		/* Send string till null */
-	{
-		USART_Transmit(str[j]);	
-		j++;
-	}
+void USART_SendString(char *str) {
+    unsigned char j = 0;
+
+    while (str[j] != 0) /* Send string till null */ {
+        USART_Transmit(str[j]);
+        j++;
+    }
 }
--- Embedded-Work/MCAL/UART/UART.h
+++ Embedded-Work/MCAL/UART/UART.h
@@ -10,12 +10,107 @@
 #include "Registers.h"
 #include "DIO.h"
 
-#define USART_BAUDRATE 103
+#define USART_BAUDRATE 9600
 #define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)	
 
-void USART_Init( uint32_t baud );
-void USART_Transmit( uint8_t data );
-uint8_t USART_Receive( void );
+/* Initializing the UART */
+#define TX_ENABLE   0
+#define RX_ENABLE 1
+#define TX_RX_ENABLE  2
+
+#define DATA_BITS_5 3
+#define DATA_BITS_6 4
+#define DATA_BITS_7 5
+#define DATA_BITS_8 6
+#define DATA_BITS_9 7
+
+#define PARITY_MODE_DISABLED    8
+#define PARITY_MODE_RESERVED    9
+#define PARITY_MODE_ENABLED_EVEN   10
+#define PARITY_MODE_ENABLED_ODD    11
+
+#define STOP_BITS_1 12
+#define STOP_BITS_2 13
+
+#define NORMAL_SPEED    14
+#define DOUBLE_SPEED    15
+
+typedef struct {
+    uint64_t baudRate;
+    uint8_t enableTXorRX;
+    uint8_t characterSize;
+    uint8_t parity;
+    uint8_t stopbitsNumber;
+    uint8_t speed;
+
+} ST_UART_config_t;
+
+typedef struct {
+    vuint8_t MPCM : 1;
+    vuint8_t U2X : 1;
+    vuint8_t PE : 1;
+    vuint8_t DOR : 1;
+    vuint8_t FE : 1;
+    vuint8_t UDRE : 1;
+    vuint8_t TXC : 1;
+    vuint8_t RXC : 1;
+
+} SUCSRA_t;
+
+typedef union {
+    vuint8_t All_Bits;
+    SUCSRA_t Ucsra;
+
+} UUCSRA_t;
+
+typedef struct {
+    vuint8_t TXB8 : 1;
+    vuint8_t RXB8 : 1;
+    vuint8_t UCSZ2 : 1;
+    vuint8_t TXEN : 1;
+    vuint8_t RXEN : 1;
+    vuint8_t UDRIE : 1;
+    vuint8_t TXCIE : 1;
+    vuint8_t RXCIE : 1;
+
+} SUCSRB_t;
+
+typedef union {
+    vuint8_t All_Bits;
+    SUCSRB_t Ucsrb;
+
+} UUCSRB_t;
+
+typedef struct {
+    vuint8_t UCPOL : 1;
+    vuint8_t UCSZ0 : 1;
+    vuint8_t UCSZ1 : 1;
+    vuint8_t USBS : 1;
+    vuint8_t UPM0 : 1;
+    vuint8_t UPM1 : 1;
+    vuint8_t UMSEL : 1;
+    vuint8_t URSEL : 1;
+
+} SUCSRC_t;
+
+typedef union {
+    vuint8_t All_Bits;
+    SUCSRC_t Ucsrc;
+
+} UUCSRC_t;
+
+#define ADDRESS_UCSRA    (UUCSRA_t*)(UCSRA)
+#define ADDRESS_UCSRB    (UUCSRB_t*)(UCSRB)
+#define ADDRESS_UCSRC    (UUCSRC_t*)(UCSRC)
+
+void UART_TX_RX_Enable (ST_UART_config_t *pUART);
+void UART_Parity (ST_UART_config_t *pUART);
+void UART_Data_Size(ST_UART_config_t *pUART);
+void UART_Stop_Bit(ST_UART_config_t *pUART);
+void UART_Speed(ST_UART_config_t *pUART);
+void UART_Initialize (uint64_t Baud_Rate);
+void USART_Transmit(uint8_t data);
+uint8_t USART_Receive(void);
 void USART_SendString(char *str);
 
 #endif	/* UART_H */
